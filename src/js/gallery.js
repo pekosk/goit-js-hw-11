003df54f95387a3fabf64200019fd3569b38dfb6
pekosk <rkostyk64@gmail.com>
2021-11-25 16:33:05 +0200

    load more не работает

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -17,31 +17,47 @@ const showLoadMoreBtn = () => {
   loadMoreBtn.classList.remove('is-hidden');
 }
 
-const increasePageInLocalStorage = () => {
-  const pageIterattor = Number(localStorage.getItem('page')) + 1;
-  localStorage.setItem('page', JSON.stringify(pageIterattor));
-};
+// const increasePageInLocalStorage = () => {
+//   const pageIterattor = Number(localStorage.getItem('page')) + 1;
+//   localStorage.setItem('page', JSON.stringify(pageIterattor));
+//   console.log(pageIterattor);
+// };
 
 const renderGalleryCards = data => {
   return data.hits
   .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="380px" height="240px"/>
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${likes}</b>
+      <b>Likes:</b> ${likes}
     </p>
     <p class="info-item">
-      <b>Views: ${views}</b>
+      <b>Views:</b> ${views}
     </p>
     <p class="info-item">
-      <b>Comments: ${comments}</b>
+      <b>Comments:</b> ${comments}
     </p>
     <p class="info-item">
-      <b>Downloads: ${downloads}</b>
+      <b>Downloads:</b> ${downloads}
     </p>
   </div>
 </div>`)
 }
+
+const onLoadMoreBtnClick = async () => {
+  const query = localStorage.getItem('query');
+
+  try {
+    const { data } = await fetchRequest(query);
+
+    galleryList.insertAdjacentHTML('beforeend', renderGalleryCards(data));
+    // increasePageInLocalStorage();
+    const pageIterattor = Number(localStorage.getItem('page')) + 1;
+    localStorage.setItem('page', JSON.stringify(pageIterattor));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const formSubmitClick = async event => {
   event.preventDefault();
@@ -57,7 +73,9 @@ const formSubmitClick = async event => {
     const { data } = await fetchRequest(query);
 
     galleryList.insertAdjacentHTML('beforeend', renderGalleryCards(data));
-    increasePageInLocalStorage();
+    // increasePageInLocalStorage();
+    const pageIterattor = Number(localStorage.getItem('page')) + 1;
+    localStorage.setItem('page', JSON.stringify(pageIterattor));
     showLoadMoreBtn();
   } catch (err) {
     console.log(err);
@@ -65,3 +83,4 @@ const formSubmitClick = async event => {
 };
 
 gallerySearch.addEventListener('submit', formSubmitClick);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
